f5c0e4d5418bc4fdbe6b6af5592ea3c494fd356d
v-kondratiev <viktor.kondratiev@gmail.com>
2023-07-05 13:22:13 +0100

    add products

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style/Base.css'
 import './App.css';
 import Product from "./Components/Product";
 
-function App() {
-  return (
-    <div className="App">
-      <Product/>
 
-    </div>
-  );
+
+
+function App() {
+    return (
+        <div className="App">
+
+            <Product/>
+
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,18 +1,52 @@
 import {useState, useEffect} from 'react';
+import * as React from 'react';
+import style from './Product.module.scss'
+
 
 const Product = () => {
 
-    const [product, getProducts] = useState([]);
+    const [products, getProducts] = useState([]);
     useEffect(() => {
-        fetch('https://648d66c52de8d0ea11e7cda6.mockapi.io/products')
+        fetch('https://648d66c52de8d0ea11e7cda6.mockapi.io/Items')
             .then(data => data.json())
             .then(result => getProducts(result))
 
     }, []);
 
+    const cards =products.map(product => (
+
+        <div className={style.Card}>
+            <div className={style.favorite} >
+
+            </div>
+
+            <img className={style.cardImage} src={product.img} alt="Sun Glasses"/>
+            <h5>{product.title}</h5>
+            {/*<h5>{product.description}</h5>*/}
+
+            <div className={style.cardBottom}>
+                <div className={style.cardPrice}>
+                    <span>Price: </span>
+                    <b>{product .price}</b>
+                </div>
+                <button className={style.addBtn} >
+                    BUY NOW
+                </button>
+            </div>
+        </div>
+
+
+    ))
+
+
+
     return (
-        <div>
+        <div className={style.product}>
             <h1>Product</h1>
+            <div className={style.items}>
+                {cards}
+            </div>
+
 
         </div>
     )
